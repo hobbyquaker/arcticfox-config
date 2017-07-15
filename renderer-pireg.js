3194f94f4491af8b19fc80b343d11a4ff0fc0798
hobbyquaker <hq@ccu.io>
2017-07-15 13:21:24 +0200

    translate pi-regulator dialog (closes #12)

--- renderer-pireg.js
+++ renderer-pireg.js
@@ -1,6 +1,10 @@
-const ipc =         require('electron').ipcRenderer;
-const $ = jQuery =  require('jquery');
-const remote =      require('electron').remote;
+const ipc = require('electron').ipcRenderer;
+const $ = jQuery = require('jquery');
+const remote = require('electron').remote;
+const app = remote.app;
+const fs = require('fs');
+const path = require('path');
+let lang = {};
 
 $('#cancel').click(function () {
     let window = remote.getCurrentWindow();
@@ -24,3 +28,21 @@ ipc.on('data', (event, data) => {
     $('#PIRegulatorPValue').val(data.PIRegulatorPValue);
     $('#PIRegulatorIValue').val(data.PIRegulatorIValue);
 });
+
+function uiTranslate() {
+    try {
+        const fp = path.join(__dirname, 'i18n', app.getLocale() + '.json');
+        lang = JSON.parse(fs.readFileSync(fp).toString());
+    } catch (err) {
+        return;
+    }
+    $('[data-lang]').each(function () {
+        const key = $(this).data('lang');
+        const phrase = lang[key];
+        if (phrase) {
+            $(this).html(phrase);
+        }
+    });
+}
+
+uiTranslate();
